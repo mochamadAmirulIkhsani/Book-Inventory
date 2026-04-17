031b126bf29eff2805aa7d824b71d86feb0a9de7
Amirul Ikhsani <amirulikhsani41@gmail.com>
2026-04-18 01:15:57 +0700

    refactor main.go to organize imports and improve code structure

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"book_inventory/app"
+	"book_inventory/db"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	conn := db.InitDB()
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	handler := app.New(conn)
+
+	// home
+	router.GET("/books", handler.GetBooks)
+
+	router.Run()
+}
