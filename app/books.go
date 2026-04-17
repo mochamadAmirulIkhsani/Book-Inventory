4dc7555df844ff0063d98642e467d543e8488c98
Amirul Ikhsani <amirulikhsani41@gmail.com>
2026-04-18 00:48:31 +0700

    implement GetBooks handler to retrieve and display book inventory

---
+++ app/books.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"book_inventory/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func New(db *gorm.DB) Handler {
+	return Handler{DB: db}
+}
+
+func (h *Handler) GetBooks(c *gin.Context) {
+	var books []models.Books
+
+	h.DB.Find(&books)
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"title":   "Home Page",
+		"payload": books,
+	})
+}
